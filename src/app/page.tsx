aa9ae852a1f6a7fd6212962fa663bfc1011ff8d2
VINCENT <vwal290@aucklanduni.ac.nz>
2025-06-05 12:47:43 +1200

    move projects to projectspage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { Hero } from "@/app/components/Hero";
 import { About } from "@/app/components/About";
-import { Projects } from "@/app/components/Projects";
 import { FindMe } from "@/app/components/FindMe";
 import { Contact } from "@/app/components/Contact";
 import { GlobalBackground } from "@/app/components/GlobalBackground";
@@ -12,7 +11,6 @@ const Home = () => {
       <Hero />
       <main className="px-4 sm:px-6 lg:px-8">
         <About />
-        <Projects />
         <FindMe />
       </main>
       <Contact />
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,11 @@
-const Projects = () => {
-  return <h1 className="heading h1">Projects</h1>;
+import { Projects } from "@/app/components/Projects";
+
+const ProjectsPage = () => {
+  return (
+    <div>
+      <h1 className="heading h1">Projects</h1>
+      <Projects />
+    </div>
+  );
 };
-export default Projects;
+export default ProjectsPage;
